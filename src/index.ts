151a1bdd805440ea070f7a3d51f640958b2d2888
ricopomps <ricardopompiliodossantos@gmail.com>
2021-04-24 20:52:30 -0300

    Express configuration an .env

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 const app = express();
-const PORT = 5000;
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+const { PORT } = process.env;
+console.log(PORT);
+console.log(process.env);
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
