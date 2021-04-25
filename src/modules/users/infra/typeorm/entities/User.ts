14bf3d6605566b96e84bf56d6d8e221ed8006bde
ricopomps <ricardopompiliodossantos@gmail.com>
2021-04-24 21:30:18 -0300

    Removed id

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -1,11 +1,8 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column } from 'typeorm';
 import { Exclude } from 'class-transformer';
 
 @Entity('users')
 class User {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-
   @Column()
   user: string;
 
