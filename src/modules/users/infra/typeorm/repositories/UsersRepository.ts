d97866b19339c43b751c6b67bfce5709bbe2b123
recabp <renatocabp@outlook.com>
2021-04-24 21:31:50 -0300

    User

---
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,22 @@
+import { getRepository, Repository } from 'typeorm';
+import IUsersRepository from '@modules/';
+
+import User from '../entities/User';
+
+class UsersRepository implements IUsersRepository {
+  private ormRepository: Repository<User>;
+
+  constructor() {
+    this.ormRepository = getRepository(User);
+  }
+
+  public async findByUser(user: string): Promise<User | undefined> {
+    const correctUser = await this.ormRepository.findOne({
+      where: { user },
+    });
+
+    return correctUser;
+  }
+}
+
+export default UsersRepository;
