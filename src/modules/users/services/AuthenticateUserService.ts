450320d394e248c299d2c59598c585a85e1d5b1e
recabp <renatocabp@outlook.com>
2021-04-25 01:11:28 -0300

    AuthenticateService

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,4 +1,10 @@
+import { inject, injectable } from 'tsyringe';
+
 import User from '@modules/users/infra/typeorm/entities/User';
+import IHashProvider from '@shared/container/providers/HashProvider/models/IHashProvider';
+
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
 
 interface IRequest {
   user: string;
@@ -9,5 +15,26 @@ interface IResponse {
   token: string;
 }
 
-class AuthenticateUserService {}
+@injectable()
+class AuthenticateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async run({ user, password }: IRequest): Promise<IResponse> {
+    const users = await this.usersRepository.findByUser(user);
+    if (!users) {
+      throw new AppError('Incorrect Email or password.', 401);
+    }
+
+    const CheckPassword = await this.hashProvider.compareHash(
+      password,
+      users.password,
+    );
+  }
+}
 export default AuthenticateUserService;
